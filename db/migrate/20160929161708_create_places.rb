400f1c2f5915e9c5decd4e01a23dc899a4518f6f
Alvin Ng <alvindng@gmail.com>
2016-09-29 11:06:09 -0700

    add static display of google maps

---
+++ db/migrate/20160929161708_create_places.rb
@@ -0,0 +1,11 @@
+class CreatePlaces < ActiveRecord::Migration[5.0]
+  def change
+    create_table :places do |t|
+      t.string :title
+      t.string :visited_by
+      t.text :address
+      t.float :latitude
+      t.float :longitude
+    end
+  end
+end
